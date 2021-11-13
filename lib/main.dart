a6af36f5cb7fb66e4cc53cef0e2350e457d1ea29
Nir Gilad <nir@vacayz.com>
2021-11-13 08:36:25 +0200

    add scrolling on the main app body

--- lib/main.dart
+++ lib/main.dart
@@ -21,18 +21,20 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter App'),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('chart placeholder'),
-                elevation: 5,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  child: Text('chart placeholder'),
+                  elevation: 5,
+                ),
               ),
-            ),
-            UserTransactions()
-          ],
+              UserTransactions()
+            ],
+          ),
         ));
   }
 }
