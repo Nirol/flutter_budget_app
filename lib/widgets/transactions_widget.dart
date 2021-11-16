4089483ff7b287d253c14a87dbc5d93b5156bcbd
Nir Gilad <nir@vacayz.com>
2021-11-16 09:07:35 +0200

    fixing chart amount title by adding height to column, also reversing genereate list of days order so the current day would be on the right

--- lib/widgets/transactions_widget.dart
+++ lib/widgets/transactions_widget.dart
@@ -54,45 +54,6 @@ class Transactions extends StatelessWidget {
                     ),
                   ),
                 );
-                //   return Card(
-                //     child: Row(
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: <Widget>[
-                //           Container(
-                //             margin: EdgeInsets.symmetric(
-                //                 vertical: 10, horizontal: 15),
-                //             decoration: BoxDecoration(
-                //               border: Border.all(color: Colors.black, width: 2),
-                //             ),
-                //             padding: EdgeInsets.all(10),
-                //             child: Text(
-                //               '\$${transacations[index].amount.toStringAsFixed(2)}',
-                //               style: TextStyle(
-                //                 fontWeight: FontWeight.bold,
-                //                 fontSize: 20,
-                //                 color: Theme.of(context).primaryColor,
-                //               ),
-                //             ),
-                //           ),
-                //           Column(
-                //             crossAxisAlignment: CrossAxisAlignment.start,
-                //             children: <Widget>[
-                //               Text(
-                //                 transacations[index].title,
-                //                 style: Theme.of(context).textTheme.headline6,
-                //               ),
-                //               Text(
-                //                 DateFormat.yMMMd()
-                //                     .format(transacations[index].date),
-                //                 style: TextStyle(
-                //                   fontWeight: FontWeight.w300,
-                //                   color: Colors.grey,
-                //                 ),
-                //               )
-                //             ],
-                //           ),
-                //         ]),
-                //   );
               },
               itemCount: transacations.length,
             ),
